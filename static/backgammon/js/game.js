07deeaf23977e5c34acbd657ea85a58c57b80933
unknown <alexmanolache92@yahoo.com>
2024-01-30 19:52:04 +0200

    drag and drop all pieces

--- static/backgammon/js/game.js
+++ static/backgammon/js/game.js
@@ -1,35 +1,47 @@
-const pct_w_5 = document.getElementById("piece_white_5");
+const pieces = document.querySelectorAll(".pieces");
 const arrows = document.querySelectorAll(".arrow");
 
-pct_w_5.addEventListener("dragstart", (event) => {
-  event.dataTransfer.setData("text/plain", pct_w_5.id);
-  event.target.classList.add('active')
-});
-
-arrows.forEach((arrow) => {
-  arrow.addEventListener("dragover", (event) => {
-    event.preventDefault();
+const movePiece = () => {
+  pieces.forEach((piece) => {
+    piece.addEventListener("dragstart", (event) => {
+      event.dataTransfer.setData("text/plain", piece.id);
+      event.target.classList.add("active");
+    });
   });
 
-  arrow.addEventListener("drop", (event) => {
-    const pieceId = event.dataTransfer.getData("text/plain");
-    const draggedPiece = document.getElementById(pieceId);
-  
-    const targetArrow = event.target.closest('.arrow');
-    if (targetArrow) {
-      const rect = targetArrow.getBoundingClientRect();
-  
-      const offsetX = event.clientX - rect.left;
-      const offsetY = event.clientY - rect.top;
-  
-      draggedPiece.style.left = offsetX + targetArrow.offsetLeft  + "px";
-      draggedPiece.style.top = offsetY + targetArrow.offsetTop + "px";
-        
-      draggedPiece.classList.add('centered')
+  arrows.forEach((arrow) => {
+    arrow.addEventListener("dragover", (event) => {
+      arrow.classList.add("cover");
+      event.preventDefault();
+    });
 
-      targetArrow.appendChild(draggedPiece);
-      draggedPiece.classList.remove('active');
-    }
+    arrow.addEventListener("dragleave", () => {
+      arrow.classList.remove("cover");
+    });
+
+    arrow.addEventListener("drop", (event) => {
+      const pieceId = event.dataTransfer.getData("text/plain");
+      const draggedPiece = document.getElementById(pieceId);
+
+      const targetArrow = event.target.closest(".arrow");
+      if (targetArrow) {
+        const rect = targetArrow.getBoundingClientRect();
+       
+        const offsetX = event.clientX - rect.left;
+        const offsetY = event.clientY - rect.top;
+
+        draggedPiece.style.left = offsetX + targetArrow.offsetLeft + "px";
+        draggedPiece.style.top = offsetY + targetArrow.offsetTop + "px";
+
+        draggedPiece.classList.add("centered");
+
+        targetArrow.appendChild(draggedPiece);
+
+        draggedPiece.classList.remove("active");
+        targetArrow.classList.remove("cover");
+      }
+    });
   });
-  
-});
+};
+
+movePiece();

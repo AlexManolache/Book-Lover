7b831d6aeb0f8ee3599e67349540c1404ec191cb
unknown <alexmanolache92@yahoo.com>
2024-02-01 01:32:44 +0200

    added methods numberPieces, checkTypePieces to add and remove class for arrow  and refactor method movePieces

--- static/backgammon/js/game.js
+++ static/backgammon/js/game.js
@@ -2,11 +2,17 @@ const whitePieces = document.querySelectorAll(".white_pieces");
 const blackPieces = document.querySelectorAll(".black_pieces");
 const arrows = document.querySelectorAll(".arrow");
 
+let numb;
+
+// no of Pieces from one arrow
+const numberPieces = (arrow) => {
+  return arrow.children.length;
+};
+
 const movePieces = (pieces) => {
   pieces.forEach((piece) => {
     piece.addEventListener("dragstart", (event) => {
       event.dataTransfer.setData("text/plain", piece.id);
-      event.target.classList.add("active");
     });
   });
 
@@ -25,7 +31,16 @@ const movePieces = (pieces) => {
       }
     });
 
+    
+    numb = numberPieces(arrow);
+
     arrow.addEventListener("dragleave", () => {
+      // -1 is subtracted from numberPieces to update the length of the previous arrow when the "dragleave" event occurs
+      numb -= 1;
+      if(numb === 0) {
+        arrow.classList.remove("has_white_pieces");
+        arrow.classList.remove("has_black_pieces");
+      }
       arrow.classList.remove("cover");
     });
 
@@ -35,26 +50,82 @@ const movePieces = (pieces) => {
 
       const targetArrow = event.target.closest(".arrow");
 
-      // check if one seat is empty or at most one seat is occupied
-      if (targetArrow.children.length <= 1) {
+      const rect = targetArrow.getBoundingClientRect();
 
-        const rect = targetArrow.getBoundingClientRect();
+      const offsetX = event.clientX - rect.left;
+      const offsetY = event.clientY - rect.top;
 
-        const offsetX = event.clientX - rect.left;
-        const offsetY = event.clientY - rect.top;
+      draggedPiece.style.left = offsetX + targetArrow.offsetLeft + "px";
+      draggedPiece.style.top = offsetY + targetArrow.offsetTop + "px";
 
-        draggedPiece.style.left = offsetX + targetArrow.offsetLeft + "px";
-        draggedPiece.style.top = offsetY + targetArrow.offsetTop + "px";
+      draggedPiece.classList.add("centered");
 
-        draggedPiece.classList.add("centered");
+      // check if targetArraw is empty, has white or black pieces to be appended corresponding pieces
+      const nameClass = checkTypePieces(targetArrow);
 
+      if (
+        nameClass == "" ||
+        nameClass == "has_white_pieces" ||
+        nameClass == "has_black_pieces"
+      ) {
         targetArrow.appendChild(draggedPiece);
-
-        draggedPiece.classList.remove("active");
+        numb = numberPieces(targetArrow);
       }
+
       targetArrow.classList.remove("cover");
     });
   });
+};
+
+// check for empty arrows and based on color of the pieces, a class will be  added / removed for arrow
+// return name of the class which was added
+const checkTypePieces = (arrow) => {
+  const nPieces = numberPieces(arrow);
+  let nameClass = "";
+  if (nPieces == 0) {
+    arrow.classList.remove("has_white_pieces");
+    arrow.classList.remove("has_black_pieces");
+    nameClass = "";
+  } else if (
+    nPieces >= 2 &&
+    arrow.children[0].classList.contains("white_pieces") &&
+    arrow.children[1].classList.contains("black_pieces")
+  ) {
+    arrow.classList.remove("has_white_pieces");
+    arrow.classList.add("has_black_pieces");
+    nameClass = "has_black_pieces";
+  } else if (
+    nPieces >= 2 &&
+    arrow.children[0].classList.contains("black_pieces") &&
+    arrow.children[1].classList.contains("white_pieces")
+  ) {
+    arrow.classList.remove("has_black_pieces");
+    arrow.classList.add("has_white_pieces");
+    nameClass = "has_white_pieces";
+  } else if (
+    nPieces == 1 &&
+    arrow.children[0].classList.contains("white_pieces")
+  ) {
+    arrow.classList.add("has_white_pieces");
+    nameClass = "has_white_pieces";
+  } else if (
+    nPieces == 1 &&
+    arrow.children[0].classList.contains("black_pieces")
+  ) {
+    arrow.classList.add("has_black_pieces");
+    nameClass = "has_black_pieces";
+  }
+
+  // Additional check to remove the class when there are no children
+  if (
+    (arrow.children.length == 0 &&
+      arrow.classList.contains("has_white_pieces")) ||
+    arrow.classList.contains("has_black_pieces")
+  ) {
+    arrow.classList.remove("has_white_pieces");
+    arrow.classList.remove("has_black_pieces");
+  }
+  return nameClass;
 };
 
 movePieces(whitePieces);
